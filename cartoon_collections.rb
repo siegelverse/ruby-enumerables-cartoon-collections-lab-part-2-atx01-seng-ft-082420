37da8810b5e3573cfefd9a6d9b36be9cb86a3d96
Marcus Siegel <msiegel37@gmail.com>
2020-08-12 00:46:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,9 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.map do |array| 
     if array.count > 4 
-      
+      array.any?
   end
+ end
 end
 
 def find_valid_calls(planeteer_calls)
