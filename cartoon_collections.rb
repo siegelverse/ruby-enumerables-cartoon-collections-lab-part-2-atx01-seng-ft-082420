c72bc3bdabf55d9c4773c0c971a913e561d80c00
Marcus Siegel <msiegel37@gmail.com>
2020-08-12 00:44:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,9 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
+  planeteer_calls.map do |array| 
+    if array.count > 4 
+  end
 end
 
 def find_valid_calls(planeteer_calls)
